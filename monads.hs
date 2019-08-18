638c59ee05d5e179661e83aac57f1b87c5f262d3
zhvrnkv <v.zhvrnkv@gmail.com>
2019-08-18 23:02:06 +0600

    Add `goto` function, that takes start position, dest position and return all variants of moves for knight (chess) from start to dest

--- monads.hs
+++ monads.hs
@@ -70,3 +70,54 @@ Interesting and tricky parts:
 1. >>> operator - why it comes with MyMonad class?
 2. do; x <- [1..50] - also tricky and fettered part. Code below that statement will be executed for each element. Need to understand clear relation between `<-` and `>==`
 -}
+
+type KnightPos = (Int, Int)
+type KnightMove = [KnightPos]
+
+moveKnight :: KnightPos -> KnightMove
+moveKnight (c,r) = do
+  (c',r') <- [(c + 2, r + 1), (c + 2, r - 1),
+              (c - 2, r + 1), (c - 2, r - 1),
+              (c + 1, r + 2), (c - 1, r + 2),
+              (c + 1, r - 2), (c - 1, r - 2)]
+  guard (c' `elem` [1..8] && r' `elem` [1..8])
+  return (c', r')
+
+in3 :: KnightPos -> KnightMove
+in3 start = do
+  first <- moveKnight start
+  second <- moveKnight first
+  moveKnight second
+
+canReactIn3 start end = end `elem` in3 start
+
+-- Implement goto function that require start position and end position. Output is most shortest path of combinations from start to dest
+goto :: [KnightMove] -> KnightPos -> [KnightMove]
+goto start dest =  
+  if null filtered then goto (moves start) dest
+  else filtered
+  where filtered = filter (\move -> (last move) == dest) start
+
+moves :: [KnightMove] -> [KnightMove]
+moves [] = []
+moves (x:xs) = getFurtherMoves x ++ moves xs
+  where getFurtherMoves = (\move ->
+          map (\newPos -> move ++ [newPos]) (moveKnight . last $ move))
+
+
+
+
+{-
+Monad Laws
+1. Left identity
+`return x >>= f` <=> `f x`
+2. Right identity
+`m >>= return` <=> `m`
+3. Associativity
+`(m >>= f) >>= g` <=> `m >>= (\x -> f x >>= g)`
+or
+`(m <==< f) <==< g` <=> m <==< (f <==< g)`
+-}
+
+-- (<==<) :: (MyMonad m) => (b -> m c) -> (a -> m b) -> (a -> m c)
+-- f <==< g = (\x -> g x >== f)
